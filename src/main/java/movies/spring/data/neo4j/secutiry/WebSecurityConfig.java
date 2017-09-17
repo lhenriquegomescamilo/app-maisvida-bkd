2fa158ce53b8a91803fce38afba83fa370829b5f
Luis Camilo <lhenriquegomescamilo@gmail.com>
2017-09-17 14:17:25 -0300

    master: configurando cors

--- src/main/java/movies/spring/data/neo4j/secutiry/WebSecurityConfig.java
+++ src/main/java/movies/spring/data/neo4j/secutiry/WebSecurityConfig.java
@@ -1,11 +1,13 @@
 package movies.spring.data.neo4j.secutiry;
 
+import movies.spring.data.neo4j.configurations.RestConfigCors;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -21,6 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, LOGIN).permitAll()
                 .anyRequest().authenticated()
                 .and()
+                .addFilterBefore(new RestConfigCors(), ChannelProcessingFilter.class)
                 .addFilterBefore(new JWTLoginFilter(LOGIN, authenticationManager()), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
